2d36fad1a90e3a6c95adaeac6212b44b03502f98
Andriyan78 <110392316+Andriyan78@users.noreply.github.com>
2023-01-05 07:56:23 +0200

    fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,14 +13,14 @@ function onFormSubmit(e) {
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
     .then(({ position, delay }) => {
-      setTimeout(() => {
+     
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      }, delay)
+      
     })
     .catch(({ position, delay }) => { 
-      setTimeout(() => {
+      
         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      }, delay) 
+      
     });
   delay += step;
   }
@@ -30,11 +30,13 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   let promiseValue = {position, delay};
 
-return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+  setTimeout(() => {
     if (shouldResolve) {
       resolve(promiseValue);
     } else {
       reject(promiseValue);
     }
+    }, delay) 
   })
 }
